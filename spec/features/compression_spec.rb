2e9738a2786bfc29b95dbdb5abb82b69b0c271ae
Dan Collis-Puro <dan@collispuro.net>
2015-03-14 10:00:01 -0400

    Add compression to rails generated responses

---
+++ spec/features/compression_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'Compression' do
+  scenario "a visitor has a browser that supports compression", :js do
+    page.driver.allow_url('filepicker.io')
+    page.driver.allow_url('googleapis.com')
+
+    ['deflate','gzip', 'deflate,gzip','gzip,deflate'].each do|compression_method|
+      Capybara.current_session.driver.header('HTTP_ACCEPT_ENCODING', compression_method )
+      visit '/'
+
+      expect(page.response_headers.keys).to include('Content-Encoding')
+    end
+  end
+end
