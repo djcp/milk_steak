ccfce2079d45a6322bac8672367fcf09b78a048e
Dan Collis-Puro <dan@collispuro.net>
2017-08-06 19:57:49 -0400

    Update dependencies

--- spec/support/features/session_helper.rb
+++ spec/support/features/session_helper.rb
@@ -5,7 +5,7 @@ module Features
       # This right here is why the build fails on integration specs sometimes
       # The user creation happens outside the main thread.
       create(:user).tap do |user|
-        user.confirm!
+        user.confirm
         visit new_user_session_path
         fill_in 'Email', with: user.email
         fill_in 'Password', with: user.password
